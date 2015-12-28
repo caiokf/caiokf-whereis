53bdc1337c251d80b437493d69ba75c30881f221
Caio Kinzel Filho <caiokf@gmail.com>
2015-12-28 11:49:26 +0200

    marker and info window in sync

--- public/js/controllers/appController.js
+++ public/js/controllers/appController.js
@@ -1,139 +1,19 @@
-angular.module('whereIsCaioKF').
+angular.module('whereIsCaioKF', ['ngMap']).
   
-  controller('AppController', function ($scope) {
+  controller('AppController', function ($scope, NgMap) {
 
-    $scope.itinerary = [
-      'Goreme, Turkey',
-      //'Goreme, Turkey',
-      //'Antalya, Turkey',
-      'Antalya, Turkey',
-      'Pamukkale, Turkey',
-      'Chios, Greece',
-      'Athens, Greece',
-      'Skala Oropou, Greece',
-      //'Trikala, Greece',
-      'Litochoro, Greece',
-      'Giannitsa, Greece',
-      //'Ulcinj, Montenegro',
-      'Dubrovnik, Croatia',
-      'Zoaric, Croatia',
-      'Jezerce, Croatia',
-      'Muggia, Italy',
-      'Morter, Italy',
-      //'Geneva, Switzerland',
-      //'Geneva, Switzerland',
-      //'Geneva, Switzerland',
-      //'Geneva, Switzerland',
-      // 'Geneva, Switzerland',
-      // //'Lucerne, Switzerland',
-      // 'Lucerne, Switzerland',
-      // //'Baar, Zug, Switzerland',
-      // 'Baar, Zug, Switzerland',
-      // 'Reichenburg, Switzerland',
-      // 'Farenstoggli, Switzerland',
-      // 'Schwagalp, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Appenzell, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Buhler, Switzerland',
-      // 'Zurich, Switzerland',
-      // 'Zurich, Switzerland',
-      // 'Zurich, Switzerland',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Canoas, Brazil',
-      // 'Arroio do Sal, Rio Grande do Sul, Brazil',
-      // 'Arroio do Sal, Rio Grande do Sul, Brazil',
-      // 'Capao da Canoa, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Montevideo, Uruguay',
-      // 'Cardona, Soriano, Uruguay',
-      // 'Cardona, Soriano, Uruguay',
-      // 'Cardona, Soriano, Uruguay',
-      // 'Cardona, Soriano, Uruguay',
-      // 'Cardona, Soriano, Uruguay',
-      // 'Montevideo, Uruguay',
-      // 'Piriapolis, Uruguay',
-      // 'Piriapolis, Uruguay',
-      // 'Maldonado, Maldonado, Uruguay',
-      // 'Maldonado, Maldonado, Uruguay',
-      // 'Maldonado, Maldonado, Uruguay',
-      // 'La Paloma, Uruguay',
-      // 'La Paloma, Uruguay',
-      // 'Barra de Valizas, Uruguay',
-      // 'Barra de Valizas, Uruguay',
-      // 'Punta del Diablo, Uruguay',
-      // 'Punta del Diablo, Uruguay',
-      // 'Fortaleza Santa Tereza, Uruguay',
-      // 'Fortaleza Santa Tereza, Uruguay',
-      // 'Fortaleza Santa Tereza, Uruguay',
-      // 'Fortaleza Santa Tereza, Uruguay',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Arroio do Meio, RS, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil',
-      // 'Porto Alegre, Brazil'
-    ];
-    $scope.cost = {
-      amount: 0,
-      currency: 'USD'
+    NgMap.getMap().then(function(map) {
+      $scope.map = map;
+    });
+
+    $scope.showInfo = function(evt, text) {
+      $scope.info = text;
+      $scope.map.showInfoWindow('info', this);
     };
 
-    // $scope.$on('mapInitialized', function(event, map) {
-    //   $scope.map = map;
-    //   map.setZoom(2);
-    //   // var markers = [];
-
-    //   // for (var x = 0; x < $scope.itinerary.length; x++) {
-    //   //   $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+$scope.itinerary[x]+'&sensor=false', null, function (data) {
-    //   //       var p = data.results[0].geometry.location
-    //   //       var latlng = new google.maps.LatLng(p.lat, p.lng);
-    //   //       var marker = new google.maps.Marker({
-    //   //           position: latlng, map: map
-    //   //       });
-    //   //       markers.push(marker);
-    //   //   });
-    //   // }
-    // });
+    $scope.itinerary = [
+      {latLong: '-30.0331, -51.2300', description: 'Porto Alegre, Brazil'},
+      {latLong: '46.2000, 6.1500', description: 'Geneva, Switzerland'},
+      {latLong: '38.6431, 34.8289', description: 'Goreme, Turkey'},
+    ];
   });
